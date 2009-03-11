a4c1f0ebefefde7240af5a3b638cea67bc3644b8
Amiel Martin <amiel.martin@gmail.com>
2009-03-11 02:45:37 -0700

    this is a start to a js refactor, right now jquery is stumping me

--- Support/lib/go_to_file.rb
+++ Support/lib/go_to_file.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/ruby
+
 require 'erb'
 
 asset_path = ENV['TM_BUNDLE_SUPPORT'] + '/assets'
+asset_path_for_html = asset_path.gsub(' ', '%20')
 
 html_path = asset_path + '/gotofile.html.erb'
-css_path = asset_path.gsub(' ', '%20') + '/gotofile.css'
-js_path = asset_path.gsub(' ', '%20') + '/gotofile.js'
+
 
 project_path = ENV['TM_PROJECT_DIRECTORY'] || ENV['TM_DIRECTORY'] || ENV['TM_FILEPATH'] && File.dirname(ENV['TM_FILEPATH'])
 
@@ -13,6 +14,7 @@ js_vars = {
   :bundle_support => ENV['TM_BUNDLE_SUPPORT'],
   :path_to_ruby => ENV['TM_RUBY'] || 'ruby',
 }.collect { |var, value| "var #{var} = '#{value}';\n" }
+
 
 
 puts ERB.new(File.read(html_path)).result
