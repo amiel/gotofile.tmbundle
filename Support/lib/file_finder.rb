e9864f7f72cd5e3a4206c99d07a4e540ed00e541
Hans-Jörg Bibiko <bibiko@eva.mpg.de>
2009-03-29 21:18:50 +0200

    • initial changes for adding a preference pane

--- Support/lib/file_finder.rb
+++ Support/lib/file_finder.rb
@@ -5,16 +5,24 @@
 
 
 require 'erb'
-
-# max number of found files after sorting by score
-MAX_OUTPUT = 100
-
 require File.dirname(__FILE__) + '/fuzzy_file_finder'
 
-TM_FUZZYFINDER_REVERSEPATHMODE = (ENV['TM_FUZZYFINDER_REVERSEPATHMODE'] and ENV['TM_FUZZYFINDER_REVERSEPATHMODE'].to_i != 0) ? true : false
-TM_FUZZYFINDER_IGNORE = ENV['TM_FUZZYFINDER_IGNORE'] ? ENV['TM_FUZZYFINDER_IGNORE'].to_s.split(/,/) : nil
-TM_FUZZYFINDER_CEILING = ENV['TM_FUZZYFINDER_CEILING'] ? ENV['TM_FUZZYFINDER_CEILING'].to_i : 10000
 
+## todo : read these values from plist
+# max number of found files after sorting by score
+max_output = 100
+# max number of files scanned
+file_ceiling = 10000
+# file globs ignored
+ignore_globs = "*.tmproj"
+# global reverse mode
+reverse_mode = 0
+
+# TM_FUZZYFINDER_REVERSEPATHMODE = (ENV['TM_FUZZYFINDER_REVERSEPATHMODE'] and ENV['TM_FUZZYFINDER_REVERSEPATHMODE'].to_i != 0) ? true : false
+# TM_FUZZYFINDER_IGNORE = ENV['TM_FUZZYFINDER_IGNORE'] ? ENV['TM_FUZZYFINDER_IGNORE'].to_s.split(/,/) : nil
+# TM_FUZZYFINDER_CEILING = ENV['TM_FUZZYFINDER_CEILING'] ? ENV['TM_FUZZYFINDER_CEILING'].to_i : 10000
+
+ignore_glob_array = (ignore_globs.empty?) ? nil : ignore_globs.to_s.split(/ *, */)
 
 project_path = ENV['TM_PROJECT_DIRECTORY'] || ENV['TM_DIRECTORY'] || ENV['TM_FILEPATH'] && File.dirname(ENV['TM_FILEPATH'])
 
@@ -31,8 +39,8 @@ if search_string.nil? || search_string.empty?
 end
 
 
-if TM_FUZZYFINDER_REVERSEPATHMODE or search_string =~ /\\(?! )/
-  TM_FUZZYFINDER_REVERSEPATHMODE = true
+if reverse_mode or search_string =~ /\\(?! )/
+  reverse_mode = true
   search_string = search_string.split(/\\(?! )/).reverse.join("/")
 end
 
@@ -46,23 +54,27 @@ template_path = asset_path + '/_file.html.erb'
 template = ERB.new(File.read(template_path))
 
 begin
-  FuzzyFileFinder.new(project_path, TM_FUZZYFINDER_CEILING, TM_FUZZYFINDER_IGNORE, FuzzyFileFinder::HtmlCharacterRun).find(search_string).sort{|b,a| a[:score] <=> b[:score] }.each do |p|
+  FuzzyFileFinder.new(
+        project_path, 
+        file_ceiling, 
+        ignore_glob_array, 
+        FuzzyFileFinder::HtmlCharacterRun).find(search_string).sort{|b,a| a[:score] <=> b[:score] }.each do |p|
     sc = (p[:score].to_f * 100).to_i
     sc = 100 if sc > 100
     hpath = p[:highlighted_path]
-    hpath = hpath.split(%r{/(?!span)}).reverse.join("&lt;") if TM_FUZZYFINDER_REVERSEPATHMODE
+    hpath = hpath.split(%r{/(?!span)}).reverse.join("&lt;") if reverse_mode
     absolute_path = p[:path]
     friendly_path = absolute_path.gsub(/^#{ENV['HOME']}/, '~')
     puts template.result(binding)
     
     cnt += 1
-    if cnt > MAX_OUTPUT
-      puts %(<p class="notice">&hellip; more than #{MAX_OUTPUT} files found.</p>)
+    if cnt > max_output
+      puts %(<p class="notice">&hellip; more than #{max_output} files found.</p>)
       break
     end
   end
 rescue FuzzyFileFinder::TooManyEntries
-  puts %(<p class="error">The root directory ‘#{project_path.gsub(/^#{ENV['HOME']}/, '~')}’ contains more than #{TM_FUZZYFINDER_CEILING} files. To increase the number of files parsed set up a TextMate shell variable <code>TM_FUZZYFINDER_CEILING</code> accordingly.</p>)
+  puts %(<p class="error">The root directory ‘#{project_path.gsub(/^#{ENV['HOME']}/, '~')}’ contains more than #{file_ceiling} files. To increase the number of files parsed set up <code>CEILING</code> in the preferences accordingly.</p>)
 rescue
   puts %(<p class="error">#{$!}</p>)  
 end
--- Support/lib/go_to_file.rb
+++ Support/lib/go_to_file.rb
@@ -7,12 +7,29 @@ asset_path_for_html = asset_path.gsub(' ', '%20')
 
 html_path = asset_path + '/gotofile.html.erb'
 
+# why do we need this (bibiko) ??
+# project_path = ENV['TM_PROJECT_DIRECTORY'] || ENV['TM_DIRECTORY'] || ENV['TM_FILEPATH'] && File.dirname(ENV['TM_FILEPATH'])
 
-project_path = ENV['TM_PROJECT_DIRECTORY'] || ENV['TM_DIRECTORY'] || ENV['TM_FILEPATH'] && File.dirname(ENV['TM_FILEPATH'])
+## todo : read these values from plist
+# max number of found files after sorting by score
+max_output = 100
+# initial search pattern
+init_search = "/"
+# max number of files scanned
+file_ceiling = 10000
+# file globs ignored
+ignore_globs = "*.tmproj"
+# global reverse mode
+reverse_mode = 0
 
 js_vars = {
   :bundle_support => ENV['TM_BUNDLE_SUPPORT'],
-  :path_to_ruby => ENV['TM_RUBY'] || 'ruby',
+  :path_to_ruby   => ENV['TM_RUBY'] || 'ruby',
+  :max_output     => max_output,
+  :init_search    => init_search,
+  :file_ceiling   =>file_ceiling,
+  :ignore_globs   => ignore_globs,
+  :reverse_mode   => reverse_mode,
 }.collect { |var, value| "var #{var} = '#{value}';\n" }
 
 
